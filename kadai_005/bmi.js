03685e0955736f05c1f18d2e1b167d779d9e381c
Daiki9029 <163009713+Daiki9029@users.noreply.github.com>
2024-04-30 20:35:01 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+let bmiNum, bodyHeight, bodyWeight, averageWeight;
+
+bodyHeight = 1.75;
+
+bodyWeight = 75;
+
+
+bmiNum = bodyWeight / (bodyHeight * bodyHeight);
+
+averageWeight = (bodyHeight * bodyHeight) * 22;
+
+console.log('私の身長は、', bodyHeight, 'mです。');
+console.log('私の体重は、', bodyWeight, 'kgです。');
+console.log('私のBMIは、', bmiNum, 'です。');
+console.log('私の適正体重は、', averageWeight, 'kgです。');
